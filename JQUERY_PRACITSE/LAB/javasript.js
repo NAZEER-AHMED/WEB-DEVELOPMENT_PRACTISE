85f68867c01dd41ac9f782d3dfabf6eaa3d74960
nazeerahmed <nazeerahmed.bcsf18@iba-suk.edu.pk>
2021-04-25 14:41:15 +0500

    changes

--- JQUERY_PRACITSE/LAB/javasript.js
+++ JQUERY_PRACITSE/LAB/javasript.js
@@ -137,3 +137,186 @@ function task6()
       }))
    }
 }
+function task8()
+{
+    let valus = $("#parms1").val().toString()
+    let total=1
+    function specialMultiply()
+    {
+        if(arguments.length===2)
+        {
+            for(let i=0;i<arguments.length;i++)
+            {
+                total*=arguments[i]
+            }
+        }
+    }
+}
+function task14(array,val)
+{
+    array.filter(itm=>{
+        return itm == val
+    })
+}
+function task12(array)
+{
+    array.map((itm,index,array)=>{
+        return itm*2;
+    })
+}
+function printFirstlast(array)
+{
+    console.log(array[array.length-1])
+    console.log(array[array[0]])
+}
+/////////////////////
+let users = [
+    {
+    username: 'larry',
+    email: 'larry@foo.com',
+    yearsExperience: 22.1,
+    favoriteLanguages: ['Perl', 'Java', 'C++'],
+    favoriteEditor: 'Vim',
+    hobbies: ['Fishing', 'Sailing', 'Hiking'],
+    hometown: {
+    city: 'San Francisco',
+    state: 'CA'
+    }
+    },
+    {
+    username: 'jane',
+    email: 'jane@test.com',
+    yearsExperience: 33.9,
+    favoriteLanguages: ['Haskell', 'Clojure', 'PHP'],
+    favoriteEditor: 'Emacs',
+    hobbies: ['Swimming', 'Biking', 'Hiking'],
+    hometown: {
+    city: 'New York',
+    state: 'NY'
+}},
+{
+username: 'sam',
+email: 'sam@test.com',
+yearsExperience: 8.2,
+favoriteLanguages: ['JavaScript', 'Ruby', 'Python', 'Go'],
+favoriteEditor: 'Atom',
+hobbies: ['Golf', 'Cooking', 'Archery'],
+hometown: {
+city: 'Fargo',
+state: 'SD'
+}
+},
+{
+username: 'anne',
+email: 'anne@test.com',
+yearsExperience: 4,
+favoriteLanguages: ['C#', 'C++', 'F#'],
+favoriteEditor: 'Visual Studio Code',
+hobbies: ['Tennis', 'Biking', 'Archery'],
+hometown: {
+city: 'Albany',
+state: 'NY'
+}
+},
+{
+username: 'david',
+email: 'david@test.com',
+yearsExperience: 12.5,
+favoriteLanguages: ['JavaScript', 'C#', 'Swift'],
+favoriteEditor: 'VS Code',
+hobbies: ['Volunteering', 'Biking', 'Coding'],
+hometown: {
+city: 'Los Angeles',
+state: 'CA'
+}
+}
+];
+function getEmails()
+{
+    let email_id = $("#emails")
+    let a =""
+    for(let i=0;i<users.length;i++)
+    {
+       a=a+" , "+users[i].email 
+       
+      
+    }
+    email_id.text(a)
+  
+}
+function hobbies()
+{
+    let hobies=$("#hobies")
+    let a=""
+    for(let i=0;i<users.length;i++)
+    {
+        a=a+" ,"+users[i].hobbies
+    }
+    hobies.text(a)
+}
+function All_lang()
+{
+    let lng = $("#lang")
+    let a=""
+    let array=[]
+    for (let i=0;i<users.length;i++)
+    {
+        for(j=0;j<users[i].favoriteLanguages.length;j++)
+        {
+            array.push(users[i].favoriteLanguages[j])
+        }
+    }
+    var uniqueArr = [...new Set(array)]; 
+    lng.text(uniqueArr)
+}
+function getbyUser()
+{
+    let user = $("#user").val()
+    let by_usr = $("#by_user")
+    for(let i=0;i<users.length;i++)
+    {
+        if(users[i].username===user)
+        {
+            by_usr.text(users[i])
+        }
+    }
+}
+function set_background()
+{
+    let get_p = $("#task20 > p")
+    for(let i=0;i<get_p.length;i++)
+    {
+        get_p.css("background-color", "yellow");
+    }
+}
+function getAttributes()
+{
+    let id1  = $("#w3r")
+    let attr = $("#attr")
+    attr.text(id1.attr("hreflang"))
+
+}
+function js_style()
+{
+    let txt = $("#text")
+    txt.css({"background-color": "yellow", "font-size": "200%"})
+}
+function insert_Row()
+{
+    let mark ="<tr><td> cell 1</td><td> cell 2 </td></tr>"
+   $("#sampleTable").append(mark)
+   
+
+}
+function task24()
+{
+
+}
+$("#colors").children().click(function(){
+
+    $(".selected").css($("#colors").children().css("background-color"))
+})
+function changeme()
+{
+    $("#change_heading").text("hello")
+}
